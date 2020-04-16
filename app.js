ea661fa1433fb1f31c7ac9c922e5144f4204e4d4
Majnuel <emacalle@hotmail.com>
2020-04-16 09:50:27 -0300

    JavScript file added

---
+++ app.js
@@ -0,0 +1,6 @@
+
+    for ( i=0 ; i<=16 ; i++ ) {
+        var grid = document.createElement("div");
+        grid.classList.add("gridStyle");
+        document.getElementById('grid-container').appendChild(grid);
+    }
